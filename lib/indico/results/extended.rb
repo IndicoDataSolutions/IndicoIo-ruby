b98265cca0d96ec4c22290a7922ee49a228f03bb
David Blackmon <davidkblackmon@gmail.com>
2014-11-18 02:46:48 -0500

    extract api_handler that returns a Results instance

--- lib/indico/results/extended.rb
+++ lib/indico/results/extended.rb
@@ -1,19 +1,28 @@
 module Indico
+  def self.results_api_handler(data, server, api)
+    data_dict = JSON.dump({'data' => data})
+    response = make_request(url_join(server, api), data_dict, HEADERS)
+    results = JSON.parse(response.body)
+    if results.key?("error")
+      results["error"]
+    else
+      Indico::Results.new results["results"]
+    end
+  end
+
   def self.language(test_text, api="remote")
-    Indico::Results.new api_handler(test_text, api, "language")
+    results_api_handler(test_text, api, "language")
   end
 
   def self.political(test_text, api="remote")
-    Indico::Results.new api_handler(test_text, api, "political")
+    results_api_handler(test_text, api, "political")
   end
 
   def self.text_tags(test_text, api="remote")
-    Indico::Results.new api_handler(test_text, api, "texttags")
+    results_api_handler(test_text, api, "texttags")
   end
 
   def self.fer(face, api="remote")
-    Indico::Results.new api_handler(face, api, "fer")
+    results_api_handler(test_text, api, "fer")
   end
 end
-
-
