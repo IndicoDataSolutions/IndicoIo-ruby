1f2ad8f84a64a1bcffc10956c3254fa151a6c9cc
Madison May <madison@indico.io>
2015-03-25 22:48:47 -0400

    Updated tests

--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Indico do
     it "should correctly parse an ini file" do 
-        Indico.load_config_files(['/home/mmay/.indicorc', './.indicorc'])
+        config = Indico.load_config_files(['/home/mmay/.indicorc', './.indicorc'])
     end
 
     it "should find the correct config files" do 
@@ -45,5 +45,27 @@ describe Indico do
 
         config = Indico.load_config()
         expect(config).to eql(expected)
+    end
+
+    it "should merge configurations properly" do 
+
+        file_username = "file-username"
+        file_password = "file-password"
+        file_cloud = "file-cloud"
+        file_config = Hash.new
+        file_config['auth'] = [file_username, file_password]
+        file_config['cloud'] = file_cloud
+
+        env_username = "env-username"
+        env_password = "env-password"
+        env_cloud = "env-cloud"
+        ENV['INDICO_USERNAME'] = env_username
+        ENV['INDICO_PASSWORD'] = env_password
+        ENV['INDICO_CLOUD'] = env_cloud
+
+        env_config = Indico.load_environment_vars()
+        merged = Indico.merge_config(file_config, env_config)
+        expect(merged['auth']).to eql([env_username, env_password])
+        expect(merged['cloud']).to eql(env_cloud)
     end
 end
