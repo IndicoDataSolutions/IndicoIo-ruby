04b289ee87275092d1342b876b482711892ece66
Anne Carlson <anne_carlson@brown.edu>
2016-06-24 14:54:32 -0400

    added tests for NER v1

--- spec/indico_spec.rb
+++ spec/indico_spec.rb
@@ -152,19 +152,25 @@ describe Indico do
   it 'should return people found in the text provided' do
     text = "Bill Gates, founder of Microsoft, can jump over a chair from standing position"
     result = Indico.people(text).sort_by { |k| -k["confidence"] }
+    v1_result = Indico.people(text, config={version: "2"}).sort_by { |k| -k["confidence"] }
     expect result[0]["text"].include? "Bill Gates"
+    expect result[0]["confidence"] != v1_result[0]["confidence"]
   end
 
   it 'should return organizations found in the text provided' do
     text = "Chinese internet giant Alibaba is to buy Hong Kong-based newspaper the South China Morning Post (SCMP)."
     result = Indico.organizations(text).sort_by { |k| -k["confidence"] }
     expect result[0]["text"].include? "Alibaba"
+    v1_result = Indico.organizations(text, config={version: "2"}).sort_by { |k| -k["confidence"] }
+    expect result[0]["confidence"] != v1_result[0]["confidence"]
   end
 
   it 'should return places found in the text provided' do
     text = "Alibaba to buy Hong Kong's South China Morning Post"
     result = Indico.places(text).sort_by { |k| -k["confidence"] }
     expect result[0]["text"].include? "China"
+    v1_result = Indico.places(text, config={version: "2"}).sort_by { |k| -k["confidence"] }
+    expect result[0]["confidence"] != v1_result[0]["confidence"]
   end
 
   it 'should tag face with correct facial expression' do
